3eebc3aa2415e80027da1a4f5b15674e1287d3fc
threeeye888 <tysonfraser6x5@gmail.com>
2021-12-07 16:20:21 -0500

    test

--- script/btcwidget.js
+++ script/btcwidget.js
@@ -1,7 +1,7 @@
  new TradingView.widget(
   {
   "width": "100%",
-  "height": "250px",  
+  "height": "300px",  
   "symbol": "BINANCE:BTCUSDT",
   "interval": "W",
   "timezone": "Etc/UTC",
@@ -11,6 +11,10 @@
   "toolbar_bg": "black",
   "enable_publishing": false,
   "allow_symbol_change": false,
+   "enable_publishing": false,
+  "hide_top_toolbar": false,
+  "withdateranges": false,
+  "save_image": false,
   
 }
   );
